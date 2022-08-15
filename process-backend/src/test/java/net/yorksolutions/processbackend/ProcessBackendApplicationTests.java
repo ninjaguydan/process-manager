79341a2b44916271392366ab139643561651923f
ninjaguydan <dthompson0190@gmail.com>
2022-08-15 15:15:00 -0500

    First commit; initial backend springboot setup

---
+++ process-backend/src/test/java/net/yorksolutions/processbackend/ProcessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.processbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
